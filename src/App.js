35145b56041bbc494d3b1df134102c4ddd743817
jensonyorke-hadley <c1014029@newcastle.ac.uk>
2026-04-29 12:38:09 +0100

    Customer frontend progress before integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import CustomerMenu from './pages/CustomerMenu';
+import DrinkOrderPage from './pages/DrinkOrderPage';
+import BasketPage from './pages/BasketPage';
+import OrderStatusPage from './pages/OrderStatusPage';
+import AccountPage from './pages/AccountPage';
+import SignInPage from './pages/SignInPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<SignInPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/order" element={<CustomerMenu />} />
+          <Route path="/drink-order/:drinkId" element={<DrinkOrderPage />} />
+          <Route path="/basket" element={<BasketPage />} />
+          <Route path="/status" element={<OrderStatusPage />} />
+          <Route path="/account" element={<AccountPage />} />
+          <Route path="/signin" element={<SignInPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
+ 
